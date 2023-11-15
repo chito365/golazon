59a29c5e7cb50f4adcae15ddbc78df08b0baf920
chito365 <weroarapmajira@gmail.com>
2023-11-15 09:56:35 +0200

    2 changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,26 @@ import LiveMatches from "components/live_matches";
 import { MAX_CACHE_TIME } from "common/config";
 
 export async function getStaticProps() {
-  return { props: {}, revalidate: MAX_CACHE_TIME };
+  // Fetch data from the external API
+  const response = await fetch("https://betadvisor.club/data/dta/b/r2.php");
+  const data = await response.json();
+
+  // Return the fetched data as props, along with revalidation time
+  return { props: { betAdvisorData: data }, revalidate: MAX_CACHE_TIME };
 }
 
-export default function IndexPage() {
+export default function IndexPage({ betAdvisorData }) {
   return (
     <Layout title={null}>
       <LiveMatches />
+      {/* Display the fetched data before the footer */}
+      <div>
+        {/* Adjust this part based on the structure of the fetched data */}
+        {betAdvisorData && betAdvisorData.someProperty && (
+          <p>{betAdvisorData.someProperty}</p>
+        )}
+      </div>
+      {/* Rest of your content, including the footer */}
     </Layout>
   );
 }
