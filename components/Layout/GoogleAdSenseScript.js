261d0ab7dc3cee176057056bf7e4db33f0de4156
chito365 <weroarapmajira@gmail.com>
2023-11-15 10:31:30 +0200

    Create GoogleAdSenseScript.js

---
+++ components/Layout/GoogleAdSenseScript.js
@@ -0,0 +1,13 @@
+// components/GoogleAdSenseScript.js
+
+import React from 'react';
+
+const GoogleAdSenseScript = () => (
+  <script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8692118545628405"
+    crossOrigin="anonymous"
+  />
+);
+
+export default GoogleAdSenseScript;
