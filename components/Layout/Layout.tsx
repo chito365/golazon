546cd708e7be8eb5e69bda4985ad71017edd4874
chito365 <weroarapmajira@gmail.com>
2023-11-15 10:27:36 +0200

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ title, header, children }: Props) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <title>{title || "Golazon"}</title>
+        <title>{title || "Foregoal Football Predictions"}</title>
 
         <meta name="theme-color" content="#ffffff" />
 
