243fe82026656712fb053b57f1377c9f81e4d050
chito365 <weroarapmajira@gmail.com>
2023-11-15 10:16:23 +0200

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -16,11 +16,6 @@ export default function Layout({ title, header, children }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <title>{title || "Foregoal Football Predictions"}</title>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8692118545628405"
-          crossOrigin="anonymous"
-        ></script>
         <meta name="theme-color" content="#ffffff" />
 
         <link rel="shortcut icon" href="/favicon.png" />
