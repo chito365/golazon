6a34d972ed748c963420606e30630d1af1b21253
chito365 <weroarapmajira@gmail.com>
2023-11-16 08:39:22 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,39 @@
 import Layout from "components/Layout";
-import LiveMatches from "components/live_matches";
-import { MAX_CACHE_TIME } from "common/config";
+import { useEffect, useState } from "react";
 
 export async function getStaticProps() {
-  return { props: {}, revalidate: MAX_CACHE_TIME };
+  // Fetch data from the external API
+  const res = await fetch("https://betadvisor.club/data/dta/b/r2.php");
+  const data = await res.json();
+
+  return {
+    props: {
+      matches: data, // Pass the fetched data as a prop
+    },
+    revalidate: 60, // Set revalidation time to 1 minute (adjust as needed)
+  };
 }
 
-export default function IndexPage() {
+export default function IndexPage({ matches }) {
   return (
     <Layout title={null}>
-      <LiveMatches />
+      <LiveMatches matches={matches} />
+      here
     </Layout>
+  );
+}
+
+// Create a new component for displaying live matches
+function LiveMatches({ matches }) {
+  return (
+    <div>
+      <h2>Live Matches</h2>
+      <ul>
+        {matches.map((match, index) => (
+          <li key={index}>{match.team1} vs {match.team2}</li>
+          // Assuming your data structure has properties like team1, team2, etc.
+        ))}
+      </ul>
+    </div>
   );
 }
