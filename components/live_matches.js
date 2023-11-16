495928adbcd9416812565003f9ed43d20118280b
chito365 <weroarapmajira@gmail.com>
2023-11-16 09:04:43 +0200

    Update live_matches.js

--- components/live_matches.js
+++ components/live_matches.js
@@ -1,36 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Fixtures, Loader } from 'components/Layout';
-import { useResource, resourcePatterns } from 'common/hyena';
-import groupFixturesByCompetitionId from 'common/util/groupFixturesByCompetitionId';
-import axios from 'axios';
+import Link from "next/link";
+import { Fixtures, Loader } from "components/Layout";
+import { useResource, resourcePatterns } from "common/hyena";
+import groupFixturesByCompetitionId from "common/util/groupFixturesByCompetitionId";
 
 export default function LiveMatches() {
   const { data: liveMatches, loading } = useResource(() =>
     resourcePatterns.liveMatches()
   );
-
-  const [additionalData, setAdditionalData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          'https://betadvisor.club/data/dta/b/data.json'
-        );
-        const data = response.data;
-
-        // Check if the "Odds" column is missing or empty
-        const filteredData = data.filter((row) => !!row[7]);
-
-        setAdditionalData(filteredData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   if (!liveMatches && loading) {
     return (
@@ -71,20 +47,6 @@ export default function LiveMatches() {
           </div>
         </div>
       ))}
-
-      {/* Display additional data at the end of Fixtures */}
-      <div id="dataContainer" className="block">
-        {additionalData.map((row, index) => (
-          <div key={index}>
-            <p>Date: {row[0]}</p>
-            <p>Match: {row[3]}</p>
-            <p>Score: {row[4]}</p>
-            <p>Bet Type: {row[6]}</p>
-            <p>Odds: {row[7]}</p>
-            <hr />
-          </div>
-        ))}
-      </div>
     </div>
   );
 }
