25ce6d14166fa2b73ed5fe1f62c2eabc24eac79e
chito365 <weroarapmajira@gmail.com>
2023-11-16 08:57:45 +0200

    Update live_matches.js

--- components/live_matches.js
+++ components/live_matches.js
@@ -1,52 +1,29 @@
-import Link from "next/link";
-import { Fixtures, Loader } from "components/Layout";
-import { useResource, resourcePatterns } from "common/hyena";
-import groupFixturesByCompetitionId from "common/util/groupFixturesByCompetitionId";
+// Import necessary TypeScript definitions for jQuery and DataTables
+import * as $ from 'jquery';
+import 'datatables.net';
 
-export default function LiveMatches() {
-  const { data: liveMatches, loading } = useResource(() =>
-    resourcePatterns.liveMatches()
-  );
+$(document).ready(() => {
+    // Read the data from the updated JSON URL
+    $.getJSON('https://betadvisor.club/data/dta/b/data.json', (data) => {
+        // Check if the "Odds" column is missing or empty
+        const filteredData = data.filter((row: any) => !!(row[7]));
 
-  if (!liveMatches && loading) {
-    return (
-      <div className="home__container container block">
-        <Loader noWrapper />
-      </div>
-    );
-  }
+        // Build the HTML table
+        const tableBody = $('#myTable tbody');
+        filteredData.forEach((row: any) => {
+            const htmlRow = `<tr>
+                                <td>${row[0]}</td>
+                                <td>${row[3]}</td>
+                                <td>${row[4]}</td>
+                                <td>${row[6]}</td>
+                                <td>${row[7]}</td>
+                            </tr>`;
+            tableBody.append(htmlRow);
+        });
 
-  const groupedMatches = liveMatches?.length
-    ? groupFixturesByCompetitionId(liveMatches)
-    : [];
-
-  if (groupedMatches.length === 0) {
-    return (
-      <div className="home__container container block">
-        No live matches at the moment.
-      </div>
-    );
-  }
-
-  return (
-    <div className="home__container container">
-      {groupedMatches.map((item) => (
-        <div key={item.competition.id}>
-          <h2>
-            <Link href={`/c/${item.competition.id}`}>
-              <a>
-                {item.competition.name}
-                {item.competition.area_name &&
-                  ` (${item.competition.area_name}) `}
-                {item.teamtype}
-              </a>
-            </Link>
-          </h2>
-          <div className="block">
-            <Fixtures fixtures={item.matches} />
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-}
+        // Initialize DataTables
+        $('#myTable').DataTable({
+            pageLength: 20, // Set the number of items per page to 20
+        });
+    });
+});
