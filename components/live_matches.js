2179b49a31f64fba7fc187d44348cbdcbac962e7
chito365 <weroarapmajira@gmail.com>
2023-11-16 09:03:12 +0200

    Update live_matches.js

--- components/live_matches.js
+++ components/live_matches.js
@@ -1,32 +1,90 @@
-// Import necessary TypeScript definitions
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Fixtures, Loader } from 'components/Layout';
+import { useResource, resourcePatterns } from 'common/hyena';
+import groupFixturesByCompetitionId from 'common/util/groupFixturesByCompetitionId';
 import axios from 'axios';
 
-document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        // Fetch data from the updated JSON URL
-        const response = await axios.get('https://betadvisor.club/data/dta/b/data.json');
+export default function LiveMatches() {
+  const { data: liveMatches, loading } = useResource(() =>
+    resourcePatterns.liveMatches()
+  );
+
+  const [additionalData, setAdditionalData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://betadvisor.club/data/dta/b/data.json'
+        );
         const data = response.data;
 
         // Check if the "Odds" column is missing or empty
-        const filteredData = data.filter((row: any) => !!(row[7]));
+        const filteredData = data.filter((row) => !!row[7]);
 
-        // Build the HTML content
-        const dataContainer = document.getElementById('dataContainer');
-        if (dataContainer) {
-            filteredData.forEach((row: any) => {
-                const divElement = document.createElement('div');
-                divElement.innerHTML = `
-                    <p>Date: ${row[0]}</p>
-                    <p>Match: ${row[3]}</p>
-                    <p>Score: ${row[4]}</p>
-                    <p>Bet Type: ${row[6]}</p>
-                    <p>Odds: ${row[7]}</p>
-                    <hr>
-                `;
-                dataContainer.appendChild(divElement);
-            });
-        }
-    } catch (error) {
+        setAdditionalData(filteredData);
+      } catch (error) {
         console.error('Error fetching data:', error);
-    }
-});
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!liveMatches && loading) {
+    return (
+      <div className="home__container container block">
+        <Loader noWrapper />
+      </div>
+    );
+  }
+
+  const groupedMatches = liveMatches?.length
+    ? groupFixturesByCompetitionId(liveMatches)
+    : [];
+
+  if (groupedMatches.length === 0) {
+    return (
+      <div className="home__container container block">
+        No live matches at the moment.
+      </div>
+    );
+  }
+
+  return (
+    <div className="home__container container">
+      {groupedMatches.map((item) => (
+        <div key={item.competition.id}>
+          <h2>
+            <Link href={`/c/${item.competition.id}`}>
+              <a>
+                {item.competition.name}
+                {item.competition.area_name &&
+                  ` (${item.competition.area_name}) `}
+                {item.teamtype}
+              </a>
+            </Link>
+          </h2>
+          <div className="block">
+            <Fixtures fixtures={item.matches} />
+          </div>
+        </div>
+      ))}
+
+      {/* Display additional data at the end of Fixtures */}
+      <div id="dataContainer" className="block">
+        {additionalData.map((row, index) => (
+          <div key={index}>
+            <p>Date: {row[0]}</p>
+            <p>Match: {row[3]}</p>
+            <p>Score: {row[4]}</p>
+            <p>Bet Type: {row[6]}</p>
+            <p>Odds: {row[7]}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
