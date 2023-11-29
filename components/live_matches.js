cea2007490ef13921a38ea2768f83aefaf7b06e6
chito365 <weroarapmajira@gmail.com>
2023-11-29 10:55:50 +0200

    Update live_matches.js

--- components/live_matches.js
+++ components/live_matches.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Fixtures, Loader } from "components/Layout";
 import { useResource, resourcePatterns } from "common/hyena";
@@ -7,6 +8,28 @@ export default function LiveMatches() {
   const { data: liveMatches, loading } = useResource(() =>
     resourcePatterns.liveMatches()
   );
+
+  // State to store additional football data
+  const [additionalFootballData, setAdditionalFootballData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://betadvisor.club/data/dta/b/data.json"
+        );
+        const jsonData = await response.json();
+
+        // Assuming jsonData is an array
+        setAdditionalFootballData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle error as needed
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs only once on mount
 
   if (!liveMatches && loading) {
     return (
@@ -29,49 +52,6 @@ export default function LiveMatches() {
       </div>
     );
   }
-
-  const additionalFootballData = [
-    [
-      "11/29 00:00",
-      "",
-      "Mexicali",
-      "Mexicali - Saltillo",
-      "1-0",
-      "Saltillo",
-      "Under 2.5",
-      "1.82",
-      "",
-      "",
-      ""
-    ],
-    [
-      "11/29 00:15",
-      "",
-      "CA Torque",
-      "CA Torque - Deportivo Maldon",
-      "5-1",
-      "Deportivo Maldon",
-      "Under 2.5",
-      "1.83",
-      "",
-      "",
-      ""
-    ],
-    [
-      "11/29 00:30",
-      "",
-      "Vasco da Gama",
-      "Vasco da Gama - Corinthians",
-      "2-4",
-      "Corinthians",
-      "Over 2.5",
-      "2.56",
-      "",
-      "",
-      ""
-    ]
-    // Add more data rows as needed
-  ];
 
   return (
     <div className="home__container container">
