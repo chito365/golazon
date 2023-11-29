9a0a50df4a1508380ef6f3768d33e096ef663b3f
chito365 <weroarapmajira@gmail.com>
2023-11-29 09:44:56 +0200

    Update live_matches.js

--- components/live_matches.js
+++ components/live_matches.js
@@ -29,24 +29,68 @@ export default function LiveMatches() {
   }
 
   return (
-    <div className="home__container container">
-      {groupedMatches.map((item) => (
-        <div key={item.competition.id}>
-          <h2>
-            <Link href={`/c/${item.competition.id}`}>
-              <a>
-                {item.competition.name}
-                {item.competition.area_name &&
-                  ` (${item.competition.area_name}) `}
-                {item.teamtype}
-              </a>
-            </Link>
-          </h2>
-          <div className="block">
-            <Fixtures fixtures={item.matches} />
+    <div>
+      {/* Existing code for grouped matches */}
+      <div className="home__container container">
+        {groupedMatches.map((item) => (
+          <div key={item.competition.id}>
+            <h2>
+              <Link href={`/c/${item.competition.id}`}>
+                <a>
+                  {item.competition.name}
+                  {item.competition.area_name &&
+                    ` (${item.competition.area_name}) `}
+                  {item.teamtype}
+                </a>
+              </Link>
+            </h2>
+            <div className="block">
+              <Fixtures fixtures={item.matches} />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+
+      {/* New div with the provided table code */}
+      <div className="home__container container">
+        <table>
+          <thead>
+            <tr>
+              <th>Kick-off</th>
+              <th>Teams</th>
+              <th>Tip</th>
+              <th>Odds</th>
+            </tr>
+          </thead>
+          <tbody id="tableBody"></tbody>
+        </table>
+
+        <script>
+          {/* Provided script for fetching and populating the table */}
+          fetch('https://betadvisor.club/data/dta/b/data.json')
+            .then(response => response.json())
+            .then(data => {
+              const tableBody = document.getElementById('tableBody');
+
+              data.forEach(row => {
+                const odds = parseFloat(row[7]);
+                const tip = row[6];
+
+                if (odds >= 2.00 && odds <= 2.60 && tip === 'Over2.5') {
+                  const tr = document.createElement('tr');
+                  tr.innerHTML = `
+                    <td>${row[0]}</td>
+                    <td>${row[3]}</td>
+                    <td>${row[6]}</td>
+                    <td>${row[7]}</td>
+                  `;
+                  tableBody.appendChild(tr);
+                }
+              });
+            })
+            .catch(error => console.error('Error fetching data:', error));
+        </script>
+      </div>
     </div>
   );
 }
