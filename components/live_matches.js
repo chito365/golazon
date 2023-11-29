4116267724aa6aea7ea58d3860ad14888f0eac32
chito365 <weroarapmajira@gmail.com>
2023-11-29 09:59:22 +0200

    Update live_matches.js

--- components/live_matches.js
+++ components/live_matches.js
@@ -22,43 +22,56 @@ export default function LiveMatches() {
     : [];
 
   if (groupedMatches.length === 0) {
+    // Display the table with the provided script when there are no live matches
     return (
       <div className="home__container container block">
         No live matches at the moment.
+
+        {/* Table with the provided script */}
+        <table>
+          <thead>
+            <tr>
+              <th>Kick-off</th>
+              <th>Teams</th>
+              <th>Tip</th>
+              <th>Odds</th>
+            </tr>
+          </thead>
+          <tbody id="tableBody"></tbody>
+        </table>
+
+        {/* Script to fetch and populate the table */}
+        <script>
+          fetch('https://betadvisor.club/data/dta/b/data.json')
+            .then(response => response.json())
+            .then(data => {
+              const tableBody = document.getElementById('tableBody');
+
+              data.forEach(row => {
+                const odds = parseFloat(row[7]);
+                const tip = row[6];
+
+                if (odds >= 2.00 && odds <= 2.60 && tip === 'Over2.5') {
+                  const tr = document.createElement('tr');
+                  tr.innerHTML = `
+                    <td>${row[0]}</td>
+                    <td>${row[3]}</td>
+                    <td>${row[6]}</td>
+                    <td>${row[7]}</td>
+                  `;
+                  tableBody.appendChild(tr);
+                }
+              });
+            })
+            .catch(error => console.error('Error fetching data:', error));
+        </script>
       </div>
     );
   }
 
-  // Use useEffect to run the script after the component has mounted
-  useEffect(() => {
-    // Provided script for fetching and populating the table
-    fetch('https://betadvisor.club/data/dta/b/data.json')
-      .then(response => response.json())
-      .then(data => {
-        const tableBody = document.getElementById('tableBody');
-
-        data.forEach(row => {
-          const odds = parseFloat(row[7]);
-          const tip = row[6];
-
-          if (odds >= 2.00 && odds <= 2.60 && tip === 'Over2.5') {
-            const tr = document.createElement('tr');
-            tr.innerHTML = `
-              <td>${row[0]}</td>
-              <td>${row[3]}</td>
-              <td>${row[6]}</td>
-              <td>${row[7]}</td>
-            `;
-            tableBody.appendChild(tr);
-          }
-        });
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  }, []); // Empty dependency array ensures this effect runs once after the initial render
-
+  // Display the grouped matches when available
   return (
     <div>
-      {/* Existing code for grouped matches */}
       <div className="home__container container">
         {groupedMatches.map((item) => (
           <div key={item.competition.id}>
@@ -77,21 +90,6 @@ export default function LiveMatches() {
             </div>
           </div>
         ))}
-      </div>
-
-      {/* New div with the provided table code */}
-      <div className="home__container container">
-        <table>
-          <thead>
-            <tr>
-              <th>Kick-off</th>
-              <th>Teams</th>
-              <th>Tip</th>
-              <th>Odds</th>
-            </tr>
-          </thead>
-          <tbody id="tableBody"></tbody>
-        </table>
       </div>
     </div>
   );
