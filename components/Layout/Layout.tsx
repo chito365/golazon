11f84459e2aff9a956f5835e65c59c416fb90008
chito365 <weroarapmajira@gmail.com>
2023-11-30 09:17:41 +0200

    gg

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,29 +1,68 @@
-// components/live_matches.js
+// components/Layout.js
 
-import { useEffect, useState } from 'react';
+import { ReactNode, StrictMode } from "react";
+import Head from "next/head";
+import SiteHead from "./SiteHead";
+import GoogleAdSenseScript from "./GoogleAdSenseScript"; // Adjust the path
+import LiveMatches from "./live_matches"; // Import the new component
 
-const LiveMatches = () => {
-  const [additionalFootballData, setAdditionalFootballData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/footballData'); // Use a serverless function to fetch data
-        const data = await response.json();
-        setAdditionalFootballData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      {/* Render your component using additionalFootballData */}
-    </div>
-  );
+type Props = {
+  title: string | false;
+  header?: string | ReactNode;
+  children?: ReactNode;
 };
 
-export default LiveMatches;
+export default function Layout({ title, header, children }: Props) {
+  return (
+    <div>
+      <Head>
+        {/* ... (existing head content) ... */}
+      </Head>
+
+      <div id="app">
+        <p className="footer container">Foregoal Free Predictions</p>
+        <p className="footer container">
+          <a href="/free/">Free</a> {" • "}
+          <a
+            href="https://www.pickskenya.online/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            VIP
+          </a>
+        </p>
+
+        <StrictMode>
+          <SiteHead />
+
+          {header && (
+            <div className="container block">
+              <h1>{header}</h1>
+            </div>
+          )}
+
+          {children}
+
+          {/* Additional Football Data */}
+          <div className="container block">
+            <h2>Additional Football Data</h2>
+            {/* Use the new LiveMatches component */}
+            <LiveMatches />
+          </div>
+        </StrictMode>
+
+        <p className="footer container">Foregoal Free Predictions</p>
+        <p className="footer container">
+          <a href="/developer/">data api</a> {" • "}
+          <a
+            href="https://www.pickskenya.online/want-to-get-paid-as-tipster/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Become a Tipster (get paid)
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
--- pages/api/footballData.js
+++ pages/api/footballData.js
@@ -1,13 +1,18 @@
-// pages/api/footballData.js
+// pages/api/footballData.ts
 
-export default async function handler(req, res) {
-    try {
-      const response = await fetch('https://betadvisor.club/data/dta/b/data.json');
-      const data = await response.json();
-      res.status(200).json(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+import { NextApiRequest, NextApiResponse } from 'next';
+
+interface AdditionalFootballData {
+  // Define your data structure here based on the actual data
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<AdditionalFootballData[]>) {
+  try {
+    const response = await fetch('https://betadvisor.club/data/dta/b/data.json');
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-  
+}
