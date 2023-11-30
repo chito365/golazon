d37a7817963906335eb5571182c4630150e1d4b8
chito365 <weroarapmajira@gmail.com>
2023-11-30 08:54:45 +0200

    Update live_matches.js

--- components/live_matches.js
+++ components/live_matches.js
@@ -90,38 +90,7 @@ export default function LiveMatches() {
           <div className="block">
             <Fixtures fixtures={item.matches} />
           </div>
-          <div className="live-matches__container">
-            <h2>Additional Football Data</h2>
-            <table className="live-match__table">
-              <thead>
-                <tr>
-                  <th>Date/Time</th>
-                  <th></th>
-                  <th>Team 1</th>
-                  <th>Match</th>
-                  <th>Score</th>
-                  <th>Team 2</th>
-                  <th>Outcome</th>
-                  <th>Odds</th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {additionalFootballData.map((rowData, index) => (
-                  <tr key={index} className="live-match__row">
-                    {rowData.map((data, dataIndex) => (
-                      <td key={dataIndex} className="live-match__data">
-                        {data}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
           </div>
-        </div>
       ))}
     </div>
   );
