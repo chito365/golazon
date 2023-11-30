4db4faa4cde5fa1a8169ed934dcefff89d7821e9
chito365 <weroarapmajira@gmail.com>
2023-11-30 09:13:24 +0200

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,80 +1,29 @@
-// components/Layout.js
+// components/live_matches.js
 
-import { ReactNode, StrictMode, useEffect, useState } from "react";
-import Head from "next/head";
-import SiteHead from "./SiteHead";
-import GoogleAdSenseScript from "./GoogleAdSenseScript";
-import axios from "axios"; // Import axios
+import { useEffect, useState } from 'react';
 
-type Props = {
-  title: string | false;
-  header?: string | ReactNode;
-  children?: ReactNode;
-};
-
-export default function Layout({ title, header, children }: Props) {
+const LiveMatches = () => {
   const [additionalFootballData, setAdditionalFootballData] = useState([]);
 
   useEffect(() => {
-    // Fetch data from the external link
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "https://betadvisor.club/data/dta/b/data.json"
-        );
-        // Set the fetched data to the state
-        setAdditionalFootballData(response.data);
+        const response = await fetch('/api/footballData'); // Use a serverless function to fetch data
+        const data = await response.json();
+        setAdditionalFootballData(data);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
       }
     };
 
-    // Call the fetchData function
     fetchData();
-  }, []); // Empty dependency array means this effect runs once when the component mounts
+  }, []);
 
   return (
     <div>
-      <Head>
-        {/* ... (existing head content) ... */}
-      </Head>
-
-      {/* ... (other content) ... */}
-
-      {/* Additional Football Data */}
-      <div className="container block">
-        <h2>Additional Football Data</h2>
-        <table className="live-match__table">
-          <thead>
-            <tr>
-              <th>Date/Time</th>
-              <th></th>
-              <th>Team 1</th>
-              <th>Match</th>
-              <th>Score</th>
-              <th>Team 2</th>
-              <th>Outcome</th>
-              <th>Odds</th>
-              <th></th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {additionalFootballData.map((rowData, index) => (
-              <tr key={index} className="live-match__row">
-                {rowData.map((data, dataIndex) => (
-                  <td key={dataIndex} className="live-match__data">
-                    {data}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      {/* ... (other content) ... */}
+      {/* Render your component using additionalFootballData */}
     </div>
   );
-}
+};
+
+export default LiveMatches;
