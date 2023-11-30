78a8665191dbbeb67f1fd484c1c088b852ccf74a
chito365 <weroarapmajira@gmail.com>
2023-11-30 09:19:07 +0200

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,13 +4,55 @@ import { ReactNode, StrictMode } from "react";
 import Head from "next/head";
 import SiteHead from "./SiteHead";
 import GoogleAdSenseScript from "./GoogleAdSenseScript"; // Adjust the path
-import LiveMatches from "./live_matches"; // Import the new component
 
 type Props = {
   title: string | false;
   header?: string | ReactNode;
   children?: ReactNode;
 };
+
+const additionalFootballData = [
+  [
+    "11/29 00:00",
+    "",
+    "Mexicali",
+    "Mexicali - Saltillo",
+    "1-0",
+    "Saltillo",
+    "Under 2.5",
+    "1.82",
+    "",
+    "",
+    "",
+  ],
+  [
+    "11/29 00:15",
+    "",
+    "CA Torque",
+    "CA Torque - Deportivo Maldon",
+    "5-1",
+    "Deportivo Maldon",
+    "Under 2.5",
+    "1.83",
+    "",
+    "",
+    "",
+  ],
+  [
+    "11/29 00:30",
+    "",
+    "Vasco da Gama",
+    "Vasco da Gama - Corinthians",
+    "2-4",
+    "Corinthians",
+    "Over 2.5",
+    "2.56",
+    "",
+    "",
+    "",
+  ],
+  // Add more data rows as needed
+];
 
 export default function Layout({ title, header, children }: Props) {
   return (
@@ -44,10 +86,36 @@ export default function Layout({ title, header, children }: Props) {
           {children}
 
           {/* Additional Football Data */}
-          <div className="container block">
+          <div className="live-matches__container">
             <h2>Additional Football Data</h2>
-            {/* Use the new LiveMatches component */}
-            <LiveMatches />
+            <table className="live-match__table">
+              <thead>
+                <tr>
+                  <th>Date/Time</th>
+                  <th></th>
+                  <th>Team 1</th>
+                  <th>Match</th>
+                  <th>Score</th>
+                  <th>Team 2</th>
+                  <th>Outcome</th>
+                  <th>Odds</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {additionalFootballData.map((rowData, index) => (
+                  <tr key={index} className="live-match__row">
+                    {rowData.map((data, dataIndex) => (
+                      <td key={dataIndex} className="live-match__data">
+                        {data}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </StrictMode>
 
