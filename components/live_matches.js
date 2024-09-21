f021ae9415fd297a8abc7bb154aacf9d9d850a21
chito365 <weroarapmajira@gmail.com>
2024-09-21 13:37:46 +0200

    Update live_matches.js

--- components/live_matches.js
+++ components/live_matches.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Fixtures, Loader } from "components/Layout";
 import { useResource, resourcePatterns } from "common/hyena";
 import groupFixturesByCompetitionId from "common/util/groupFixturesByCompetitionId";
+import LiveScore from './LiveScore'; // Adjust the path as necessary
 
 export default function LiveMatches() {
   const { data: liveMatches, loading } = useResource(() =>
@@ -24,62 +25,12 @@ export default function LiveMatches() {
     return (
       <div className="home__container container block">
         <div className="live-matches__container">
-        <div className={style.main}>
-      <IframeResizer
-        heightCalculationMethod="lowestElement"
-        inPageLinks
-        frameBorder="0"
-        src="https://www.scorebat.com/embed/livescore/"
-        className={style.iFrame}
-      />
-    </div>
+          <div>No matches to display</div>
+          <LiveScore /> {/* Insert LiveScore component here */}
         </div>
       </div>
     );
   }
-
-  const additionalFootballData = [
-    [
-      "11/29 00:00",
-      "",
-      "Mexicali",
-      "Mexicali - Saltillo",
-      "1-0",
-      "Saltillo",
-      "Under 2.5",
-      "1.82",
-      "",
-      "",
-      ""
-    ],
-    [
-      "11/29 00:15",
-      "",
-      "CA Torque",
-      "CA Torque - Deportivo Maldon",
-      "5-1",
-      "Deportivo Maldon",
-      "Under 2.5",
-      "1.83",
-      "",
-      "",
-      ""
-    ],
-    [
-      "11/29 00:30",
-      "",
-      "Vasco da Gama",
-      "Vasco da Gama - Corinthians",
-      "2-4",
-      "Corinthians",
-      "Over 2.5",
-      "2.56",
-      "",
-      "",
-      ""
-    ]
-    // Add more data rows as needed
-  ];
 
   return (
     <div className="home__container container">
@@ -98,7 +49,7 @@ export default function LiveMatches() {
           <div className="block">
             <Fixtures fixtures={item.matches} />
           </div>
-          </div>
+        </div>
       ))}
     </div>
   );
